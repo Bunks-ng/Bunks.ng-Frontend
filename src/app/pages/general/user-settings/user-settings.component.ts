10c630aa0fe07a85ba4e44c2cb7d3d12c88d0950
clintonpi <nkwochaclinton@gmail.com>
2019-12-26 13:26:22 +0100

    implement "user profile update" feature

--- src/app/pages/general/user-settings/user-settings.component.ts
+++ src/app/pages/general/user-settings/user-settings.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { firebase } from 'firebaseui-angular';
+
 import { AppService } from 'src/app/services/app.service';
 import { scrollTop } from 'src/app/utils/scroll-to-top';
 
@@ -9,9 +12,14 @@ import { scrollTop } from 'src/app/utils/scroll-to-top';
 })
 export class UserSettingsComponent implements OnInit {
 
-  constructor(
-    private appService: AppService
-  ) { 
+  private user: firebase.User;
+  private firstName: string = '';
+  private lastName: string = '';
+  private email: string = '';
+  private profileUpdateMessage: string = '';
+  private emailUpdateMessage: string = '';
+
+  constructor(private appService: AppService, private router: Router) { 
     this.appService.loadScriptPage(this.scripts);
     scrollTop();
   }
@@ -21,8 +29,60 @@ export class UserSettingsComponent implements OnInit {
     './assets/js/ext/plugins.js',
     './assets/js/ext/scripts.js',
   ];
-  
+
+  updateProfile(event) {
+    event.preventDefault();
+
+    this.firstName = this.firstName.trim();
+    this.lastName = this.lastName.trim();
+
+    this.profileUpdateMessage = 'Updating names...';
+
+    if (!this.firstName || !this.lastName) {
+      this.profileUpdateMessage = 'Please, fill in names.';
+      return;
+    }
+
+    this.user.updateProfile({
+      displayName: this.firstName + ' ' + this.lastName,
+    }).then(() => {
+      this.profileUpdateMessage = 'Names updated successfully.';
+    }).catch((error) => {
+      this.profileUpdateMessage = error;
+    });
+  }
+
+  updateEmail(event) {
+    event.preventDefault();
+
+    this.emailUpdateMessage = 'Updating email address...';
+
+    if (!this.email) {
+      this.profileUpdateMessage = 'Please, fill in names.';
+      return;
+    }
+
+    this.user.updateEmail(this.email).then(() => {
+      this.emailUpdateMessage = 'Email address updated successfully.';
+    }).catch((error) => {
+      this.emailUpdateMessage = error;
+    });
+  }
+
   ngOnInit() {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        const { displayName, email } = user;
+        const names = displayName.split(' ');
+        
+        this.user = user;
+        this.firstName = names[0];
+        this.lastName = names[1];
+        this.email = email;
+      } else {
+        this.router.navigate(['/auth/sign-in']);
+      }
+    });
   }
 
 }
